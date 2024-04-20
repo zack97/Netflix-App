1352ad93c6c23feccd63ccac23fbcbf379d41251
zack10 <zacksmetz@yahoo.com>
2024-04-21 00:24:07 +0200

    netflix app done 1

---
+++ netflix-clone/src/Nav.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img src="/netflixicon.png" alt="Netflix Logo" className="nav__logo" />
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Avatar"
+        className="nav__avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
